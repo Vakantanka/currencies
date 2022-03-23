1a2c920c4842cc8ef359111b4fa7e81bfef9a1bf
Vakantanka <laszlo.fenyvesi@gmail.com>
2022-03-23 15:23:51 +0100

    majdnemelfejeltettemmenteni

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
-const movies = [
+const currencies = [
     {
-        title: "Youth",
-        sub: "2015",
-        text: "A retired orchestra conductor is on vacation with his daughter and his film director best friend in the Alps when he receives an invitation from Queen Elizabeth II to perform for Prince Philip's birthday."
+        title: "US Dollar",
+        sub: "$",
+        text: "USD"
     },
     {
-        title: "American Honey",
-        sub: "2016",
-        text: "A teenage girl with nothing to lose joins a traveling magazine sales crew, and gets caught up in a whirlwind of hard partying, law bending and young love as she criss-crosses the Midwest with a band of misfits."
+        title: "Euro",
+        sub: "€",
+        text: "EUR"
     },
     {
-        title: "Blue is the warmest color",
-        sub: "2013",
-        text: "Adèle's life is changed when she meets Emma, a young woman with blue hair, who will allow her to discover desire and to assert herself as a woman and as an adult. In front of others, Adèle grows, seeks herself, loses herself, and ultimately finds herself through love and loss."
+        title: "Chinese Yuan",
+        sub: "¥",
+        text: "CNY"
     },
     {
-        title: "Moonrise Kingdom",
-        sub: "2012",
-        text: "A pair of young lovers flee their New England town, which causes a local search party to fan out to find them."
+        title: "Thai Baht",
+        sub: "฿",
+        text: "THB"
     },
     {
-        title: "Howl's Moving Castle",
-        sub: "2004",
-        text: "When an unconfident young woman is cursed with an old body by a spiteful witch, her only chance of breaking the spell lies with a self-indulgent yet insecure young wizard and his companions in his legged, walking castle."
+        title: "British Pound Sterling",
+        sub: "£",
+        text: "GBP"
     },
     {
-        title: "Three Billboards Outside Ebbing, Missouri",
-        sub: "2017",
-        text: "A mother personally challenges the local authorities to solve her daughter's murder when they fail to catch the culprit."
+        title: "Ghanaian Cedi",
+        sub: "₵",
+        text: "GHS"
     },
     {
-        title: "Loro",
-        sub: "2018",
-        text: "An escort service owner uses his girls to get close to politician/PM and media mogul, Silvio Berlusconi. But in 2006, he's preoccupied with his shaky marriage and losing an election."
+        title: "Japanese Yen",
+        sub: "￥",
+        text: "JPY"
     },
     {
-        title: "Parasite",
-        sub: "2019",
-        text: "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan."
+        title: "Polish Zloty",
+        sub: "zł",
+        text: "PLN"
     }
 ]
 
 function loadEvent() {
     console.log("Load completed.");
-    document.getElementById("movies").insertAdjacentHTML("beforeend", movies.map((movie, index) => 
+    document.getElementById("currencies").insertAdjacentHTML("beforeend", currencies.map((currency, index) => 
         `<div class="card">
-            <div class="movieData">
-                <div class="index">${index + 1}</div>
-                <h2 class="title">${movie.title}</h2>
-                <div class="sub">${movie.sub}</div>
-                <div class="text">${movie.text}</div>
-            </div>
-            <button>Watch</button>
+            <div class="sub">${currency.sub}</div>
+            <div class="title">${currency.title}</div>
+            <div class="text">${currency.text}</div>
+            <button>
+                <span class="buttonText">Save</span>
+                <span class="arrow material-icons">arrow_forward</span>
+            </button>
         </div>`).join(''));
 }
 
